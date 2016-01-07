7b8d9a2abe6822c2fc2fb522b20a8421206ed064
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2016-01-07 00:40:20 -0500

    add jsdocs to functions in site.js

--- site.js
+++ site.js
@@ -1,21 +1,31 @@
 $(function() {
 
-    // Select Box 1
-      $('#select-box-1 select')
-        .on('change', function(){
+    /**
+    * Select Box 0 - No JavaScript!
+    */
+
+
+    /**
+    * Select Box 1 - Update the text of the styled overlay with 
+    * the value selected 
+    */
+    $('#select-box-1 select').on('change', function(){
         $self=$(this);
         $('#select-box-1 .select-overlay span').text($self.val());
-      });
+    });
 
 
-    // Select Box 2
+    /**
+    * Select Box 2 - Create a new list of options based on the list
+    * of real <option> elements, and show it on click of the menu. 
+    * Listen for clicks on the new options, close the menu, and 
+    * update the value of the <select>. Also listen for clicks on 
+    * the body to close the menu  
+    */
     var $selectBox2 = $('#select-box-2 select'),
         $fakeSelect = $('.fake-select'),
         $list = $('.options'),
         numberOfOptions = $('#select-box-2 select').children('option').length;
-
-    // Show the first select option in the styled div
-    // $fakeSelect.text($selectBox2.children('option').eq(0).text());
 
     for (var i = 0; i < numberOfOptions; i++) {
         $('<li />', {
